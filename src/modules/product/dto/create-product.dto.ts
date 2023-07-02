e6921d0b1716d6e705c59259a54b44debec2f210
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 19:19:33 -0300

    feat: adding class validator to dto product

--- src/modules/product/dto/create-product.dto.ts
+++ src/modules/product/dto/create-product.dto.ts
@@ -1,1 +1,22 @@
-export class CreateProductDto {}
+import {
+  IsNotEmpty,
+  IsString,
+  IsOptional,
+  MaxLength,
+  IsNumber,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @IsOptional()
+  @MaxLength(300)
+  @IsString()
+  description?: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  price: number;
+}
