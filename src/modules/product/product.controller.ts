f21439ea940ab977b4de4318e87da372a0b37813
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 19:19:11 -0300

    feat: product module implemented

--- src/modules/product/product.controller.ts
+++ src/modules/product/product.controller.ts
@@ -1,7 +1,52 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Put,
+  Delete,
+  UseGuards,
+  ParseIntPipe,
+  Param,
+} from '@nestjs/common';
 import { ProductService } from './product.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { AuthGuard } from 'src/middlewares/auth/auth.guard';
+import { Public } from 'src/middlewares/auth/decorators';
+import { UpdateProductDto } from './dto/update-product.dto';
 
-@Controller('product')
+@Controller('products')
+@UseGuards(AuthGuard)
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
+
+  @Post()
+  create(@Body() createProductDto: CreateProductDto) {
+    return this.productService.create(createProductDto);
+  }
+
+  @Public()
+  @Get()
+  findAll() {
+    return this.productService.findAll();
+  }
+
+  @Public()
+  @Get(':id')
+  findOne(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.findOne(id);
+  }
+
+  @Put(':id')
+  update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() updateProductDto: UpdateProductDto,
+  ) {
+    return this.productService.update(id, updateProductDto);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.delete(id);
+  }
 }
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,4 +1,36 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
 
 @Injectable()
-export class ProductService {}
+export class ProductService {
+  constructor(private prismaService: PrismaService) {}
+
+  async create(createProductDto: CreateProductDto) {
+    const product = await this.prismaService.product.create({
+      data: createProductDto,
+    });
+
+    return product;
+  }
+
+  async findAll() {
+    return await this.prismaService.product.findMany();
+  }
+
+  async findOne(id: number) {
+    return await this.prismaService.product.findUnique({ where: { id } });
+  }
+
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    return await this.prismaService.product.update({
+      where: { id },
+      data: updateProductDto,
+    });
+  }
+
+  async delete(id: number) {
+    return await this.prismaService.product.delete({ where: { id } });
+  }
+}
