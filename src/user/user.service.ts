cf7b9f9a1f74e3973d8bf9f654b550079df20b56
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 16:33:08 -0300

    feat: create and login routes implemented

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,11 +1,24 @@
 import { Injectable } from '@nestjs/common';
-import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { CreateUserDto } from './dtos/create-user.dto';
+import { UpdateUserDto } from './dtos/update-user.dto';
+import { PrismaService } from 'src/database/prisma/prisma.service';
+import { AuthService } from './auth/auth.service';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    private prisma: PrismaService,
+    private authService: AuthService,
+  ) {}
+
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await this.authService.hashPassword(
+      createUserDto.password,
+    );
+
+    return this.prisma.user.create({
+      data: { ...createUserDto, password: hashedPassword },
+    });
   }
 
   findAll() {
