cf559f277a27275e01a6ae29722fda57ab92f62b
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 17:59:15 -0300

    feat: cleaning routes unused

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -16,24 +16,10 @@ export class UserService {
       createUserDto.password,
     );
 
-    return this.prisma.user.create({
+    await this.prisma.user.create({
       data: { ...createUserDto, password: hashedPassword },
     });
-  }
 
-  findAll() {
-    return `This action returns all user`;
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
-  }
-
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+    return [{ ...createUserDto, password: '-' }];
   }
 }
