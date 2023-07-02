4f52ed0717cee17ac9f7f063375d3504b38a236e
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 16:34:01 -0300

    feat: new modules imported

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { PrismaModule } from 'src/database/prisma/prisma.module';
+import { AuthModule } from './auth/auth.module';
+import { AuthService } from './auth/auth.service';
+import { JwtService } from '@nestjs/jwt';
 
 @Module({
   controllers: [UserController],
-  providers: [UserService]
+  providers: [UserService, AuthService, JwtService],
+  imports: [PrismaModule, AuthModule],
 })
 export class UserModule {}
