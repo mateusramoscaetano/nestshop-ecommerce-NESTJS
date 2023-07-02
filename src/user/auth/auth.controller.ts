7f048a67f175b5e51f988c10eba44aa057f66c69
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-02 16:35:24 -0300

    feat: auth module created and configurated

---
+++ src/user/auth/auth.controller.ts
@@ -0,0 +1,14 @@
+import { AuthService } from './auth.service';
+import { LoginUserDto } from '../dtos/login-user.dto';
+import { Body, Controller, Post, HttpCode, HttpStatus } from '@nestjs/common';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @HttpCode(HttpStatus.OK)
+  @Post('login')
+  signIn(@Body() signInDto: LoginUserDto) {
+    return this.authService.signIn(signInDto);
+  }
+}
