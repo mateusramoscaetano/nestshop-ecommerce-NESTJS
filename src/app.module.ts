42fdf3bdc91e8c6d2e117e24acf06bb688c8cdf6
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-04 08:22:30 -0300

    feat: jwt process env

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,17 @@ import { ProductModule } from './modules/product/product.module';
 import { OrderModule } from './modules/order/order.module';
 import { PrismaModule } from './database/prisma/prisma.module';
 import { AuthModule } from './middlewares/auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [UserModule, ProductModule, OrderModule, PrismaModule, AuthModule],
+  imports: [
+    UserModule,
+    ProductModule,
+    OrderModule,
+    PrismaModule,
+    AuthModule,
+    ConfigModule.forRoot(),
+  ],
   providers: [],
 })
 export class AppModule {}
