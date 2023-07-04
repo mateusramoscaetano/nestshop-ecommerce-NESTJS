1344d4e9e9ae02d49b02d0f04babfabc4207c06f
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-04 12:36:31 -0300

    feat: userProvider on product

--- src/modules/product/product.module.ts
+++ src/modules/product/product.module.ts
@@ -5,10 +5,18 @@ import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { AuthModule } from 'src/middlewares/auth/auth.module';
 import { AuthService } from 'src/middlewares/auth/auth.service';
 import { AuthGuard } from 'src/middlewares/auth/auth.guard';
+import { UserProvider } from '../user/user.provider';
+import { PrismaService } from 'src/database/prisma/prisma.service';
 
 @Module({
   controllers: [ProductController],
-  providers: [ProductService, AuthService, AuthGuard],
+  providers: [
+    ProductService,
+    PrismaService,
+    AuthService,
+    AuthGuard,
+    UserProvider,
+  ],
   imports: [PrismaModule, AuthModule],
 })
 export class ProductModule {}
--- src/modules/product/product.service.ts
+++ src/modules/product/product.service.ts
@@ -1,13 +1,45 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from 'src/database/prisma/prisma.service';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
+import { UserProvider } from '../user/user.provider';
+import { GetUserDto } from '../user/dtos/get-user.dto';
 
 @Injectable()
 export class ProductService {
-  constructor(private prismaService: PrismaService) {}
+  constructor(
+    private prismaService: PrismaService,
+    private userProvider: UserProvider,
+  ) {}
+
+  private get user(): GetUserDto {
+    return this.userProvider.user;
+  }
 
   async create(createProductDto: CreateProductDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: this.user.sub },
+      include: {
+        roles: {
+          include: {
+            permissions: true,
+          },
+        },
+      },
+    });
+
+    const hasCreateProductPermission = user.roles.some((role) =>
+      role.permissions.some(
+        (permission) => permission.action === 'create-product',
+      ),
+    );
+
+    if (!hasCreateProductPermission) {
+      throw new UnauthorizedException(
+        'User does not have the "create-product" permission',
+      );
+    }
+
     const product = await this.prismaService.product.create({
       data: createProductDto,
     });
@@ -24,6 +56,29 @@ export class ProductService {
   }
 
   async update(id: number, updateProductDto: UpdateProductDto) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: this.user.sub },
+      include: {
+        roles: {
+          include: {
+            permissions: true,
+          },
+        },
+      },
+    });
+
+    const hasCreateProductPermission = user.roles.some((role) =>
+      role.permissions.some(
+        (permission) => permission.action === 'update-product',
+      ),
+    );
+
+    if (!hasCreateProductPermission) {
+      throw new UnauthorizedException(
+        'User does not have the "update-product" permission',
+      );
+    }
+
     return await this.prismaService.product.update({
       where: { id },
       data: updateProductDto,
@@ -31,6 +86,29 @@ export class ProductService {
   }
 
   async delete(id: number) {
+    const user = await this.prismaService.user.findUnique({
+      where: { id: this.user.sub },
+      include: {
+        roles: {
+          include: {
+            permissions: true,
+          },
+        },
+      },
+    });
+
+    const hasCreateProductPermission = user.roles.some((role) =>
+      role.permissions.some(
+        (permission) => permission.action === 'delete-product',
+      ),
+    );
+
+    if (!hasCreateProductPermission) {
+      throw new UnauthorizedException(
+        'User does not have the "delete-product" permission',
+      );
+    }
+
     return await this.prismaService.product.delete({ where: { id } });
   }
 }
