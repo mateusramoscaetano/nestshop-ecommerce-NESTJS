c75e7bcefd5992954047d526457227294d407497
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-04 07:35:16 -0300

    feat: order functions implemented

--- src/modules/order/order.service.ts
+++ src/modules/order/order.service.ts
@@ -1,29 +1,129 @@
-import { Injectable } from '@nestjs/common';
-import { CreateOrderDto } from './dtos/create-order.dto';
-import { UpdateOrderDto } from './dtos/update-order.dto';
+import { Injectable, BadRequestException } from '@nestjs/common';
+import { OrderDto } from './dtos/create-order.dto';
 import { PrismaService } from 'src/database/prisma/prisma.service';
+import { UserProvider } from '../user/user.provider';
+import { Order, PaymentStatus } from '@prisma/client';
+import { GetUserDto } from '../user/dtos/get-user.dto';
 
 @Injectable()
 export class OrderService {
-  constructor(private prisma: PrismaService) {}
+  constructor(
+    private prisma: PrismaService,
+    private userProvider: UserProvider,
+  ) {}
 
-  async create() {
-    return 'ok';
+  private get user(): GetUserDto {
+    return this.userProvider.user;
   }
 
-  findAll() {
-    return `This action returns all order`;
+  async create(order: OrderDto) {
+    const { productsId } = order;
+
+    const productIdCounts = productsId.reduce((counts, productId) => {
+      counts[productId] = (counts[productId] || 0) + 1;
+      return counts;
+    }, {});
+
+    const products = await this.prisma.product.findMany({
+      where: {
+        id: {
+          in: productsId,
+        },
+      },
+    });
+
+    let totalPrice = 0;
+    const connectedProducts = products.map((product) => {
+      const count = productIdCounts[product.id] || 1;
+      totalPrice += product.price * count;
+      return { id: product.id };
+    });
+
+    const createdOrder = await this.prisma.order.create({
+      data: {
+        userId: this.user.sub,
+        products: {
+          connect: connectedProducts,
+        },
+      },
+    });
+
+    return {
+      order: createdOrder,
+      totalPrice,
+    };
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} order`;
+  async update(order: Order) {
+    const orderUpdate = await this.prisma.order.update({
+      where: {
+        id: order.id,
+      },
+      data: {
+        status: order.status,
+      },
+    });
+
+    return orderUpdate;
   }
 
-  update(id: number, updateOrderDto: UpdateOrderDto) {
-    return `This action updates a #${id} order`;
+  async cancel(id: number) {
+    const orders = this.prisma.order
+      .update({ where: { id: id }, data: { status: PaymentStatus.CANCELLED } })
+      .then(async () => {
+        return this.prisma.order.findMany({
+          where: {
+            userId: this.user.sub,
+          },
+          include: {
+            products: true,
+          },
+          orderBy: {
+            createdAt: 'desc',
+          },
+        });
+      })
+      .catch((error) => {
+        throw new BadRequestException(error);
+      });
+
+    return orders;
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} order`;
+  async clearAll() {
+    await this.prisma.order.deleteMany({
+      where: {
+        userId: this.user.sub,
+      },
+    });
+    return this.findAll();
+  }
+
+  async findAll() {
+    const { sub } = this.userProvider.user;
+
+    const orders = this.prisma.order.findMany({
+      where: {
+        userId: sub,
+      },
+      include: {
+        products: true,
+      },
+      orderBy: {
+        createdAt: 'desc',
+      },
+    });
+
+    return orders;
+  }
+
+  async findOne(id: number) {
+    const order = this.prisma.order.findUnique({
+      where: {
+        id: this.user.sub,
+      },
+    });
+
+    return order;
   }
 }
