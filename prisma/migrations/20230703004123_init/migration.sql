a5f59cc942b128152b51584b115d54529fa3098e
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-04 07:31:09 -0300

    feat: adding migrations

---
+++ prisma/migrations/20230703004123_init/migration.sql
@@ -0,0 +1,57 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Product" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "price" DOUBLE PRECISION NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+    "quantity" INTEGER NOT NULL,
+
+    CONSTRAINT "Product_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Order" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "totalPrice" DOUBLE PRECISION NOT NULL,
+    "createAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updateAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Order_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_OrderProduct" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OrderProduct_AB_unique" ON "_OrderProduct"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OrderProduct_B_index" ON "_OrderProduct"("B");
+
+-- AddForeignKey
+ALTER TABLE "Order" ADD CONSTRAINT "Order_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OrderProduct" ADD CONSTRAINT "_OrderProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OrderProduct" ADD CONSTRAINT "_OrderProduct_B_fkey" FOREIGN KEY ("B") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ prisma/migrations/20230703144425_/migration.sql
@@ -0,0 +1,69 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `createAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `totalPrice` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Order` table. All the data in the column will be lost.
+  - You are about to drop the column `createAt` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `quantity` on the `Product` table. All the data in the column will be lost.
+  - You are about to drop the column `updateAt` on the `Product` table. All the data in the column will be lost.
+  - You are about to alter the column `price` on the `Product` table. The data in that column could be lost. The data in that column will be cast from `DoublePrecision` to `Integer`.
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - You are about to drop the `_OrderProduct` table. If the table is not empty, all the data it contains will be lost.
+  - Added the required column `updatedAt` to the `Order` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updatedAt` to the `Product` table without a default value. This is not possible if the table is not empty.
+  - Made the column `description` on table `Product` required. This step will fail if there are existing NULL values in that column.
+  - Added the required column `updatedAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- CreateEnum
+CREATE TYPE "PaymentStatus" AS ENUM ('CREATED', 'CONFIRMED', 'CANCELLED', 'DELIVERED');
+
+-- DropForeignKey
+ALTER TABLE "_OrderProduct" DROP CONSTRAINT "_OrderProduct_A_fkey";
+
+-- DropForeignKey
+ALTER TABLE "_OrderProduct" DROP CONSTRAINT "_OrderProduct_B_fkey";
+
+-- AlterTable
+ALTER TABLE "Order" DROP COLUMN "createAt",
+DROP COLUMN "totalPrice",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "status" "PaymentStatus" NOT NULL DEFAULT 'CREATED',
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Product" DROP COLUMN "createAt",
+DROP COLUMN "quantity",
+DROP COLUMN "updateAt",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL,
+ALTER COLUMN "description" SET NOT NULL,
+ALTER COLUMN "price" SET DATA TYPE INTEGER;
+
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "updatedAt" TIMESTAMP(3) NOT NULL;
+
+-- DropTable
+DROP TABLE "_OrderProduct";
+
+-- CreateTable
+CREATE TABLE "_OrderToProduct" (
+    "A" INTEGER NOT NULL,
+    "B" INTEGER NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_OrderToProduct_AB_unique" ON "_OrderToProduct"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_OrderToProduct_B_index" ON "_OrderToProduct"("B");
+
+-- AddForeignKey
+ALTER TABLE "_OrderToProduct" ADD CONSTRAINT "_OrderToProduct_A_fkey" FOREIGN KEY ("A") REFERENCES "Order"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_OrderToProduct" ADD CONSTRAINT "_OrderToProduct_B_fkey" FOREIGN KEY ("B") REFERENCES "Product"("id") ON DELETE CASCADE ON UPDATE CASCADE;
