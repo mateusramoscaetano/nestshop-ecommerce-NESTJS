fdfd00826d2e2b66f33bfb122152c64521cc99eb
mateusramoscaetano <mateus-ramoshd@hotmail.com>
2023-07-20 17:51:28 -0300

    feat: import updated

--- src/middlewares/auth/auth.controller.ts
+++ src/middlewares/auth/auth.controller.ts
@@ -1,24 +1,13 @@
 import { AuthService } from './auth.service';
 import { LoginUserDto } from 'src/modules/user/dtos/login-user.dto';
 import { Body, Controller, Post } from '@nestjs/common';
-import { ApiTags, ApiOkResponse } from '@nestjs/swagger';
 
 @Controller('auth')
-@ApiTags('auth')
 export class AuthController {
   constructor(private authService: AuthService) {}
 
   @Post('login')
-  @ApiOkResponse({
-    description: 'Login successful',
-    schema: {
-      properties: {
-        access_token: { type: 'string' },
-        redirect: { type: 'string' },
-      },
-    },
-  })
-  signIn(@Body() signInDto: LoginUserDto) {
-    return this.authService.signIn(signInDto);
+  signIn(@Body() loginBankDto: LoginUserDto) {
+    return this.authService.signIn(loginBankDto);
   }
 }
